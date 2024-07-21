cfcd03e40c19b00caa9e8575ee8df14ce03af615
Mati220186 <mateusz_silski@o2.pl>
2024-07-22 00:20:26 +0200

    task6-2th commit

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+const input = document.querySelector("#controls input");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+createButton.addEventListener("click", (event) => {
+  const amount = input.value;
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ``;
+  } else {
+    alert("Podaj liczbę z zakresu od 1 do 100");
+  }
+});
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+  input.value = "";
+}
+destroyButton.addEventListener("click", destroyBoxes);
