82f2a61963b1a1a85a79002f210852fb0f6a5ffe
Mati220186 <mateusz_silski@o2.pl>
2024-07-21 22:47:08 +0200

    task4 commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", submit);
+function submit(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+  const newForm = {
+    email: email,
+    password: password,
+  };
+  console.log(newForm);
+  form.reset();
+}
