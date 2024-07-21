3a026c9ca2fe22b8580fe5f3562868efb5543b44
Mati220186 <mateusz_silski@o2.pl>
2024-07-21 23:29:47 +0200

    task5 commit

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const btn = document.querySelector("body");
+const color = document.querySelector(".color");
+changeColor.addEventListener("click", (event) => {
+  const newColor = getRandomHexColor();
+  console.log(newColor);
+  btn.style.backgroundColor = newColor;
+  color.innerHTML = `${newColor}`;
+});
